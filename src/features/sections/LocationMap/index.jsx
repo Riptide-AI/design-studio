99f84c909823b59bb48d19bfe54679d5ab26250e
Maksym Vizniak <max@flixforge.com>
2024-04-03 11:31:36 +0300

    added burger menu, minor fixes, map fix

--- src/features/sections/LocationMap/index.jsx
+++ src/features/sections/LocationMap/index.jsx
@@ -3,7 +3,9 @@ import styles from './style.module.scss';
 
 export const LocationMap = () => {
   const lvivCoordinates = useMemo(() => ({ lat: 49.8397, lng: 24.0297 }), []);
+
   useEffect(() => {
+    // Define any initialization logic here
     if (typeof window !== 'undefined' && typeof window.google !== 'undefined') {
       const map = new window.google.maps.Map(document.getElementById('map'), {
         center: lvivCoordinates,
@@ -18,10 +20,17 @@ export const LocationMap = () => {
     }
   }, [lvivCoordinates]);
 
+  // Define initMap function before the Google Maps API script is loaded
+
   return (
     <div>
       <div id="map" className={styles.map}></div>
-      <script defer src="https://maps.googleapis.com/maps/api/js?callback=initMap"/>
+      <script
+        async
+        defer
+        src={`https://maps.googleapis.com/maps/api/js?callback=initMap`}
+        type="text/javascript"
+      />
     </div>
   );
 };
