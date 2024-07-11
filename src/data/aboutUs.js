29e5cd46566148ca6cc4d78544ba743f4247fff1
Haskarchik <vovakryt24@gmail.com>
2024-07-11 20:59:42 +0300

    Reasons add mob text

--- src/data/aboutUs.js
+++ src/data/aboutUs.js
@@ -43,6 +43,51 @@ export const reasons = {
       desc: 'reasons.items.6.desc',
     },
   ],
+};export const reasons_mob = {
+  title: 'reasons_mob.title',
+  image: '/img/reasons_mob.png',
+  items: [
+    {
+      title: 'reasons_mob.items.1.title',
+      desc: 'reasons_mob.items.1.desc',
+      icons: ['/img/Why_choose_us/money.png', '/img/Why_choose_us/money_currency.png'],
+      styles: ['money', 'money_currency'],
+    },
+    {
+      title: 'reasons_mob.items.2.title',
+      icons: ['/img/Why_choose_us/bulb.png', '/img/Why_choose_us/bulb_light.png'],
+      styles: ['bulb', 'bulb_light'],
+      desc: 'reasons_mob.items.2.desc',
+    },
+    {
+      title: 'reasons_mob.items.3.title',
+      icons: ['/img/Why_choose_us/computer.png', '/img/Why_choose_us/computer_gear.png'],
+      styles: ['computer', 'computer_gear'],
+      desc: 'reasons_mob.items.3.desc',
+    },
+    {
+      title: 'reasons_mob.items.4.title',
+      icons: [
+        '/img/Why_choose_us/head.png',
+        '/img/Why_choose_us/cloud.png',
+        '/img/Why_choose_us/lightning.png',
+      ],
+      styles: ['head', 'cloud', 'lightning'],
+      desc: 'reasons_mob.items.4.desc',
+    },
+    {
+      title: 'reasons_mob.items.5.title',
+      icons: ['/img/Why_choose_us/badge1.png', '/img/Why_choose_us/badge_bottom.png'],
+      styles: ['badge', 'badge_bottom'],
+      desc: 'reasons_mob.items.5.desc',
+    },
+    {
+      title: 'reasons_mob.items.6.title',
+      icons: ['/img/Why_choose_us/frame.png', '/img/Why_choose_us/content.png'],
+      styles: ['frame', 'content'],
+      desc: 'reasons_mob.items.6.desc',
+    },
+  ],
 };
 export const ourClients = {
   title: 'Наші клієнти',
